d4d234f7d5b920148fa78c10c53bdcc3e0785728
Deniz Tohumcu <deniz.tohumcu93@gmail.com>
2021-03-25 03:24:02 +0100

    build.sh fixed

--- basicExamples/build.sh
+++ basicExamples/build.sh
@@ -2,11 +2,14 @@
 
 # Hello World build commands
 
+
 as hello.s -o hello.o
 ld hello.o -o hello
 EXEC=./hello
-if test -f "$FILE"; then
+
+if test -f $EXEC ; then
     echo "Build is succesfull, executable $EXEC generated."
 else 
     echo "Build failed, executable $EXEC couldn't be generated."
 fi
+
